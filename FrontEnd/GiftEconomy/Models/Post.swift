2456aef8721b2cadb251d1a9a78bdacbaddd4040
Alexander Frazis <alexanderfrazis@gmail.com>
2020-08-22 20:08:49 +0800

    First Commit

---
+++ FrontEnd/GiftEconomy/Models/Post.swift
@@ -0,0 +1,11 @@
+//
+//  Post.swift
+//  GiftEconomy
+//
+//  Created by Alexander Frazis on 22/8/20.
+//  Copyright © 2020 CITS3200Gang. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
